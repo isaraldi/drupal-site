2f59bc6497fe41cdda626a7fde3c844be81b204a
John Murowaniecki <jmurowaniecki@gmail.com>
2024-08-18 22:43:16 -0300

    Allow the admin to see last messages sent.

--- web/modules/custom/telemetry/src/Form/TelemetryForm.php
+++ web/modules/custom/telemetry/src/Form/TelemetryForm.php
@@ -7,17 +7,27 @@ namespace Drupal\telemetry\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\telemetry\Module as Module;
+use Drupal\Core\Database\Database;
 
 /**
  * Provides a Telemetry form.
  */
 final class TelemetryForm extends FormBase {
 
+  private $connection;
+
   /**
    * {@inheritdoc}
    */
   public function getFormId(): string {
     return 'telemetry_telemetry';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct() {
+    $this->connection = Database::getConnection();
   }
 
   /**
@@ -42,16 +52,32 @@ final class TelemetryForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state): array {
+    $query = $this->connection->select('telemetry', 't')
+      ->fields('t', ['message', 'message_type'])
+      ->orderBy('id', 'DESC')
+      ->range(0, 5);
+    $restoring = $query->execute()->fetchAll();
+    $elements  = sizeof($restoring);
+    foreach ($restoring as $result) {
+      $this->messenger()->{$result->message_type}($result->message);
+    }
+
     Module::telemetry('Status', $this, 'Accessing method *'.__METHOD__.'*.');
-    return $this->getFields([
-      'actions' => [
-        '#type'  => 'actions',
-        'submit' => [
-          '#type'  => 'submit',
-          '#value' => $this->t('Send'),
-        ],
-      ]
-    ]);
+
+    $form['info'] = [
+      '#type' => 'item',
+      '#title' => t('Last sent telemetry messages'),
+      '#markup' => "See above {$elements} element".($elements > 1 ? 's' : '').' sent previously.',
+    ];
+
+    $form['actions'] = [
+      '#type'  => 'actions',
+      'submit' => [
+        '#type'  => 'submit',
+        '#value' => $this->t('Send'),
+      ],
+    ];
+    return $this->getFields($form);
   }
 
   /**
@@ -74,13 +100,24 @@ final class TelemetryForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state): void {
-    Module::telemetry(
-      $form_state->getValue('message_type'),
-      $this,
-      'Accessing method *'.__METHOD__.'*. ',
-      $form_state->getValue('message')
-    );
-    $this->messenger()->addStatus($this->t('The message has been sent.'));
+    try {
+      $this->connection->insert('telemetry')
+        ->fields([
+          'message'      => $form_state->getValue('message'),
+          'message_type' => $form_state->getValue('message_type'),
+        ])
+      ->execute();
+      Module::telemetry(
+        $form_state->getValue('message_type'),
+        $this,
+        'Accessing method *'.__METHOD__.'*. ',
+        $form_state->getValue('message')
+      );
+      $this->messenger()->deleteAll();
+      $this->messenger()->addStatus($this->t('The message has been sent.'));
+    } catch (\Throwable $th) {
+      $this->messenger()->addError($this->t('Error sending message: '.$th->getMessage()));
+    }
     $form_state->setRedirect('<front>');
   }
 
